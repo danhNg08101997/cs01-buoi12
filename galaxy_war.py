8509d4619585002e670197a8a76c7838db6b4cc0
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-01-29 06:22:19 +0700

    update sounds and images

---
+++ galaxy_war.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+
+# Khai báo cửa sổ
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption('Galaxy War')
+
+# Xây dựng player (surface: hình, rect, sound...)
+player_img = pygame.image.load('./galaxy war/graphics/air_craft.png')
+player_rect = player_img.get_rect()
+player_rect.x = SCREEN_WIDTH  / 2 - player_rect.width / 2
+player_rect.y = SCREEN_HEIGHT - 200
+
+# Vòng lặp game
+running = True
+while running:
+    screen.fill((255,255,255))
+    # Xây dựng event thoát game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # Xử lý blit các surface
+    screen.blit(player_img, player_rect)
+    
+    # Cập nhật game
+    pygame.display.flip()
+
+# Thoát khỏi vòng lặp thì tắt game
+pygame.quit()
