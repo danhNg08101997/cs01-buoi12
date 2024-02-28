8951b4589dbed5269120a6179f46afdef4a38320
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-02-29 06:43:28 +0700

    done game

--- galaxy_war.py
+++ galaxy_war.py
@@ -23,20 +23,31 @@ bg_game = pygame.image.load('./galaxy war/graphics/background_space.jpg')
 bg_game = pygame.transform.scale(bg_game, (SCREEN_WIDTH,SCREEN_HEIGHT))
 
 # Tạo 1 collection chứa đạn
-lst_bullet = []
+lst_bullet:list[pygame.Rect] = []
     # tốc độ của đạn
 speed_bullet = 2
 
 # Tạo ra 1 list chứa nhiều thiên thạch
 meteor_image = pygame.image.load('./galaxy war/graphics/meteor.png')
     # collection chứa thiên thạch
-lst_meteor = []
+lst_meteor:list[pygame.Rect] = []
     # tốc độ rơi của thiên thạch
 speed_meteor = 1
     # thời gian tạo thiên thạch
 time_meteor_start = 0
     # thời gian rơi
 time_span_meteor = 1000
+
+# Tạo biến lưu trữ
+score = 0
+level = 1
+live = 5
+
+# Xử lý âm thanh
+sound_bg = pygame.mixer.Sound('./galaxy war/sounds/music.wav')
+attack = pygame.mixer.Sound('./galaxy war/sounds/laser.ogg')
+explosion = pygame.mixer.Sound('./galaxy war/sounds/explosion.wav')
+sound_bg.play(-1)
 
 # Vòng lặp game
 running = True
@@ -64,6 +75,8 @@ while running:
             # lưu đạn vào lst_bullet
             lst_bullet.append(bullet_rect)
             print('lst_bullet: ',lst_bullet)
+            # Mỗi lần bắn thì phát ra âm thanh
+            attack.play()
             
     # Xử lý thiên thạch
     current_time_meteor = pygame.time.get_ticks() #lấy thời gian game hiện tại
@@ -77,26 +90,6 @@ while running:
         # Gán lại thời gian start
         time_meteor_start = current_time_meteor
         
-    # Xử lý blit các surface
-    screen.blit(player_img, player_rect) #1. truyền tuple tọa độ, 2. truyền vào rect
-    
-    #Xử lý surface text
-    # Mạng
-    live_text, live_rect = create_surface_text(f_game,f'Live: {5}','Red', 'White',90)
-    live_rect = (0, SCREEN_HEIGHT - 100)
-    
-    # Điểm
-    score_text, score_rect = create_surface_text(f_game, f'Score: {0}','Red', 'White', 90)
-    score_rect = (SCREEN_WIDTH / 2 - score_text.get_width() / 2,SCREEN_HEIGHT - 100)
-    
-    # Level
-    level_text, level_rect = create_surface_text(f_game, f'Level: {1}', 'Red', 'White', 90)
-    level_rect = (SCREEN_WIDTH - level_rect.width, SCREEN_HEIGHT - 100)
-    
-    screen.blit(live_text, live_rect)
-    screen.blit(score_text, score_rect)
-    screen.blit(level_text,level_rect)
-    
     # xử lý đạn
     for bullet_rect in lst_bullet:
         screen.blit(bullet_image,bullet_rect)
@@ -105,7 +98,19 @@ while running:
         # tối ưu
         if bullet_rect.y < 0:
             lst_bullet.remove(bullet_rect)
-        
+        # xét xem từng viên đạn có chạm vào thiên thạch hay không
+        for meteor_rect in lst_meteor:
+            if bullet_rect.colliderect(meteor_rect):
+                explosion.play()
+                score += 100
+                lst_meteor.remove(meteor_rect)
+                lst_bullet.remove(bullet_rect)
+                # Xử lý lên level
+                if score % 1000 == 0 and score != 0:
+                    level += 1
+                    # Tăng độ khó game
+                    time_span_meteor -= 100
+    
     # Xử lý thiên thạch rơi
     for meteor_rect in lst_meteor:
         screen.blit(meteor_image, meteor_rect)
@@ -115,6 +120,34 @@ while running:
         # tối ưu thiên thạch
         if meteor_rect.x < 0 or meteor_rect.x > SCREEN_WIDTH or meteor_rect.y > SCREEN_HEIGHT:
             lst_meteor.remove(meteor_rect)
+        # Xử lý khi thiên thạch rơi trúng máy bay
+        if meteor_rect.colliderect(player_rect):
+            live -= 1
+            # Xóa thiên thạch
+            lst_meteor.remove(meteor_rect)
+            if live < 0:
+                score = 'Game over'
+                
+    # Xử lý blit các surface
+    screen.blit(player_img, player_rect) #1. truyền tuple tọa độ, 2. truyền vào rect
+    
+    #Xử lý surface text
+    # Mạng
+    live_text, live_rect = create_surface_text(f_game,f'Live: {live}','Red', 'White',90)
+    live_rect = (0, SCREEN_HEIGHT - 100)
+    
+    # Điểm
+    score_text, score_rect = create_surface_text(f_game, f'Score: {score}','Red', 'White', 90)
+    score_rect = (SCREEN_WIDTH / 2 - score_text.get_width() / 2,SCREEN_HEIGHT - 100)
+    
+    # Level
+    level_text, level_rect = create_surface_text(f_game, f'Level: {level}', 'Red', 'White', 90)
+    level_rect = (SCREEN_WIDTH - level_rect.width, SCREEN_HEIGHT - 100)
+    
+    screen.blit(live_text, live_rect)
+    screen.blit(score_text, score_rect)
+    screen.blit(level_text,level_rect)
+    
     # Cập nhật game
     pygame.display.flip()
 
